c59a70cf3c5df4f8d44b0b6409ce690dc0722a78
tarstayu <79341083+tarstayu@users.noreply.github.com>
2021-03-12 03:55:34 +0700

    commit v.final
    add keydown and animation button

--- index.js
+++ index.js
@@ -2,37 +2,56 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var curr = this.innerHTML;
     console.log(curr)
-    switch (curr) {
-      case "w":
-        var audio1 = new Audio("sounds/tom-1.mp3");
-        audio1.play();
-        break;
-      case "a":
-        var audio2 = new Audio("sounds/tom-2.mp3");
-        audio2.play();
-        break;
-      case "s":
-        var audio3 = new Audio("sounds/tom-3.mp3");
-        audio3.play();
-        break;
-      case "d":
-        var audio4 = new Audio("sounds/tom-4.mp3");
-        audio4.play();
-        break;
-      case "j":
-        var audio5 = new Audio("sounds/snare.mp3");
-        audio5.play();
-        break;
-      case "k":
-        var audio6 = new Audio("sounds/crash.mp3");
-        audio6.play();
-        break;
-      case "l":
-        var audio7 = new Audio("sounds/kick-bass.mp3");
-        audio7.play();
-        break;
-      default:
+    makeSound(curr);
+    buttonAnimation(curr);
 
-    }
   })
+}
+
+document.addEventListener("keydown",function(event){
+  console.log(event.key);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function buttonAnimation(curr){
+  var activeButton = document.querySelector("."+curr);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio1 = new Audio("sounds/tom-1.mp3");
+      audio1.play();
+      break;
+    case "a":
+      var audio2 = new Audio("sounds/tom-2.mp3");
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio("sounds/tom-3.mp3");
+      audio3.play();
+      break;
+    case "d":
+      var audio4 = new Audio("sounds/tom-4.mp3");
+      audio4.play();
+      break;
+    case "j":
+      var audio5 = new Audio("sounds/snare.mp3");
+      audio5.play();
+      break;
+    case "k":
+      var audio6 = new Audio("sounds/crash.mp3");
+      audio6.play();
+      break;
+    case "l":
+      var audio7 = new Audio("sounds/kick-bass.mp3");
+      audio7.play();
+      break;
+    default:
+  }
 }
